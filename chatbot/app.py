603a5c2715bb33bb77127cbdf3ee281da7ad67e9
goerlitzmj <mgoer8@sas.upenn.edu>
2023-09-20 19:28:55 -0400

    moving over files

---
+++ chatbot/app.py
@@ -0,0 +1,133 @@
+# Chatbot GUI with Tkinter
+
+from tkinter import *
+from chat import get_response, bot_name
+
+BG_GREEN = "#A8B289"
+BG_COLOR = "#17202A"
+TEXT_COLOR = "#EAECEE"
+
+FONT = "Helvetica 14"
+FONT_BOLD = "Helvetica 13 bold"
+
+class ChatApplication:
+    def __init__(self):
+        self.window = Tk()
+        self._setup_main_window()
+
+    def run(self):
+        self.window.mainloop()
+
+    def _setup_main_window(self):
+        self.window.title("Chat")
+        self.window.resizable(width=False, height=False)
+        self.window.configure(width=470, height=550, bg=BG_COLOR)
+
+        # head label
+        head_label = Label(
+            self.window, 
+            bg=BG_COLOR, 
+            fg=TEXT_COLOR,
+            text="Welcome",
+            font=FONT_BOLD,
+            pady=10
+            )
+        head_label.place(relwidth=1)
+
+        # tiny divider
+        line = Label(
+            self.window,
+            width=450,
+            bg=BG_GREEN
+            )
+        line.place(relwidth=1, rely=0.07, relheight=0.012)
+
+        # text widget (use self. because want to call it later)
+        self.text_widget = Text(
+            self.window, 
+            width=20, 
+            height=2, 
+            bg=BG_COLOR,
+            fg=TEXT_COLOR, 
+            font=FONT,
+            padx=5, 
+            pady=5
+            )
+
+        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+        self.text_widget.configure(cursor="arrow", state=DISABLED)
+
+        # scroll bar
+        scrollbar = Scrollbar(self.text_widget) # relative to text_widget
+        scrollbar.place(relheight=1, relx=0.974)
+        scrollbar.configure(command=self.text_widget.yview)
+
+        # bottom label
+        bottom_label = Label(self.window, bg=BG_GREEN, height=80)
+        bottom_label.place(relwidth=1, rely=0.82)
+
+        # message entry box
+        self.msg_entry = Entry(
+            bottom_label, 
+            bg="#2C3E50", 
+            fg=TEXT_COLOR,
+            font=FONT)
+        self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+        self.msg_entry.focus()
+        self.msg_entry.bind("<Return>", self._on_enter_pressed)
+
+        # send button
+        send_button = Button(
+            bottom_label, 
+            text="Send", 
+            font=FONT_BOLD, 
+            width=20,
+            bg=BG_GREEN,
+            command=lambda:self._on_enter_pressed(None)
+            )
+        send_button.place(relx=0.77, rely=0.008, relheight=0.04, relwidth=0.22)
+
+        # clear button
+        clear_button = Button(
+            bottom_label,
+            text="Clear",
+            font=FONT_BOLD,
+            width=20,
+            bg=BG_GREEN,
+            command=lambda:self._clear_messages()
+        )
+        clear_button.place(relx=0.77, rely=0.002, relheight=0.02, relwidth=0.22)
+
+    def _on_enter_pressed(self, event):
+        msg = self.msg_entry.get()
+        self._insert_message(msg, "You")
+
+    def _insert_message(self, msg, sender):
+        if not msg:
+            return
+        self.msg_entry.delete(0, END)
+        # message from sender
+        msg1 = f"{sender}: {msg}\n\n"
+        # Need to enable state for this function
+        self.text_widget.configure(state=NORMAL)
+        self.text_widget.insert(END,msg1)
+        self.text_widget.configure(state=DISABLED)
+
+        # message from bot
+        msg2 = f"{bot_name}: {get_response(msg)}\n\n"
+        # Need to enable state for this function
+        self.text_widget.configure(state=NORMAL)
+        self.text_widget.insert(END,msg2)
+        self.text_widget.configure(state=DISABLED)
+
+        self.text_widget.see(END)
+
+    def _clear_messages(self):
+        # clear messages
+        self.text_widget.configure(state=NORMAL)
+        self.text_widget.delete('1.0', END)
+        self.text_widget.configure(state=DISABLED)
+
+if __name__ == "__main__":
+    app = ChatApplication()
+    app.run()
